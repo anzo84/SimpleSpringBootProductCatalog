78bcf042e3373daab406000be34885f6fd3befc9
Lezhnin Anton <anton.intel.tech@gmail.com>
2020-05-15 13:12:05 +0300

    Fix warnings

--- src/main/java/online/wgear/test/spring_boot_lezhnin/dao/CatalogRepository.java
+++ src/main/java/online/wgear/test/spring_boot_lezhnin/dao/CatalogRepository.java
@@ -3,11 +3,9 @@ package online.wgear.test.spring_boot_lezhnin.dao;
 import online.wgear.test.spring_boot_lezhnin.model.Catalog;
 import online.wgear.test.spring_boot_lezhnin.model.CatalogList;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CatalogRepository extends JpaRepository<Catalog,Long> {
-    //CatalogList findAllByParentIs(Catalog parent);
     CatalogList findByParentIs(Catalog parent);
 }
